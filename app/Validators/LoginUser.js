fa78111f3f31da9fa99593bd0c876b18833e6be5
stizzle123 <arifayanidowu2@yahoo.com>
2019-01-12 20:49:36 +0100

    Update LoginUser.js

--- app/Validators/LoginUser.js
+++ app/Validators/LoginUser.js
@@ -1,11 +1,25 @@
-'use strict'
+"use strict";
 
 class LoginUser {
-  get rules () {
+  get rules() {
     return {
       // validation rules
-    }
+      email: "required|email",
+      password: "required"
+    };
+  }
+
+  get messages() {
+    return {
+      required: "{{ field }} is required"
+    };
+  }
+
+  async fails(error) {
+    this.ctx.sessions.withErrors(error).flashAll();
+
+    return this.ctx.response.redirect("back");
   }
 }
 
-module.exports = LoginUser
+module.exports = LoginUser;
